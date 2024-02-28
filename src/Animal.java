b9d7e7daa640315ce499c49150f18cf190c72a1e
Maroua Taamalli <147505604+Divaminxy@users.noreply.github.com>
2024-02-28 09:28:24 +0100

    Update Animal.java
    nouveau prosit

--- src/Animal.java
+++ src/Animal.java
@@ -1,16 +1,53 @@
+package tn.esprit.gestionzoo.entitees;
+
 public class Animal {
-    String family;
-    String name;
-    int age;
-    boolean isMammal;
+    private String family; // L'attribut est maintenant privé
+    private String name;
+    private int age;
+    private boolean isMammal;
 
-    public Animal() {
-
-    }
+    // Constructeur de la classe tn.esprit.gestionzoo.entitees.Animal
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
-        this.age = age;
+        setAge(age); // Utilisation de la méthode setAge pour garantir un âge non négatif
         this.isMammal = isMammal;
+    }
+
+    public boolean isMammal() {
+        return isMammal;
+    }
+    public void setMammal(boolean mamal){
+        isMammal=mamal;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    // Méthode publique pour définir l'âge avec une vérification de non-négativité
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("L'âge ne peut pas être négatif. La valeur par défaut de 0 sera utilisée.");
+
+        }
+    }
+
+    // Méthode publique pour obtenir l'âge
+    public int getAge() {
+        return age;
+    }
+
+    // Autres getters et setters pour les attributs name, family et isMammal
+
+    @Override
+    public String toString() {
+        return "tn.esprit.gestionzoo.entitees.Animal{" +
+                "family='" + family + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isMammal=" + isMammal +
+                '}';
     }
 }
